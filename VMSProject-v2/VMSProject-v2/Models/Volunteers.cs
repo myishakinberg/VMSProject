14b59842fe780e692a0e392eb81f49bea9edc051
myishakinberg <mskinberg@gmail.com>
2018-08-21 21:49:38 -0400

    Added dummy data to manage volunteers

--- VMSProject-v2/VMSProject-v2/Models/Volunteers.cs
+++ VMSProject-v2/VMSProject-v2/Models/Volunteers.cs
@@ -7,12 +7,31 @@ namespace VMSProjectV2.Models
 {
     public class Volunteers
     {
+        public VolunteerViewModel Myisha = new VolunteerViewModel();
+
         public Volunteers()
         {
-            VolunteerViewModel Myisha = new VolunteerViewModel();
             Myisha.FirstName = "Myisha";
             Myisha.LastName = "Kinberg";
             Myisha.Username = "Mkinberg";
+            Myisha.Password = "Password";
+            Myisha.Centers.Add("Humane Society");
+            Myisha.Skills.Add("Leadership");
+            Myisha.AvailabilityDays.Add("Saturday");
+            Myisha.AvailabilityTimes.Add("noon - 5pm");
+            Myisha.Address = "120 UNF Drive";
+            Myisha.City = "Jax";
+            Myisha.State = "Florida";
+            Myisha.Zip = 1234;
+            Myisha.HomePhone = 9876543210;
+            Myisha.Email = "mkinberg@gmail.com";
+            Myisha.Education.Add("Associates");
+            Myisha.Licenses.Add("None");
+            Myisha.DriversLicence = true;
+            Myisha.SSN = true;
+            Myisha.ApprovalStatus.Add("Approved");
+            Myisha.EmergencyName = "Megan Fern";
+            Myisha.EmergencyEmail = "megs@gmail.com";
         }
     }
 }
--- VMSProject-v2/VMSProject-v2/Startup.cs
+++ VMSProject-v2/VMSProject-v2/Startup.cs
@@ -21,6 +21,8 @@ namespace VMSProject_v2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
+            services.AddSession();
             services.AddMvc();
         }
 
@@ -38,7 +40,7 @@ namespace VMSProject_v2
             }
 
             app.UseStaticFiles();
-
+            app.UseSession();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
